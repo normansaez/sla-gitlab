84a4122a17854a73ecc2c4eea8caa54720d73547
Norman Saez <norman.saez@gmail.com>
2024-06-22 01:12:15 -0400

    app updated with tickets abiertos

--- app.py
+++ app.py
@@ -15,4 +15,18 @@ GITLAB_BASE_URL = os.getenv('GITLAB_BASE_URL')
 if not GITLAB_TOKEN or not GITLAB_PROJECT_ID or not GITLAB_API_URL or not GITLAB_BASE_URL:
     raise ValueError("Una o más variables de entorno no están configuradas correctamente.")
 
+import subprocess
+
+# Botón para ejecutar el script
+if st.button("Ejecutar Script"):
+    result = subprocess.run(["python", "tickets_abiertos.py"], capture_output=True, text=True)
+    
+    # Mostrar salida y errores
+    st.write("Salida del Script:")
+    st.text(result.stdout)
+    if result.stderr:
+        st.error("Errores del Script:")
+        st.text(result.stderr)
+    else:
+        st.success("Script ejecutado con éxito")
 
